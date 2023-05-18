2cdfccdf2251572d2ed0c2cd86991f198aa5183b
Ashish Baranwal <ashish.82983858@gmail.com>
2023-05-19 00:39:44 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+class Diff{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int mult = a * b;
+        System.out.println("The difference is " + mult);
+    }
+}
